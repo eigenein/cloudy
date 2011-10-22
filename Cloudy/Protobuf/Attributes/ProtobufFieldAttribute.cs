1731d73f5540f126eb1d0b67083b2c77527f4259
Pavel Perestoronin <eigenein@gmail.com>
2011-10-23 00:40:19 +0300

    commented

--- Cloudy/Protobuf/Attributes/ProtobufFieldAttribute.cs
+++ Cloudy/Protobuf/Attributes/ProtobufFieldAttribute.cs
@@ -3,6 +3,9 @@ using Cloudy.Protobuf.Enums;
 
 namespace Cloudy.Protobuf.Attributes
 {
+    /// <summary>
+    /// Marks a property as a serializable Protobuf field.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Property, AllowMultiple=false, Inherited=true)]
     public class ProtobufFieldAttribute : Attribute
     {
@@ -14,6 +17,13 @@ namespace Cloudy.Protobuf.Attributes
 
         private readonly DataType dataType;
 
+        /// <summary>
+        /// Initializes a new instance of the attribute.
+        /// </summary>
+        /// <param name="fieldNumber">The field number of the property in a target message.</param>
+        /// <param name="dataType">Defines a way to serialize the property value.</param>
+        /// <param name="packed">Defines whether a collection should be packed in a target message.</param>
+        /// <param name="required">Marks the property as required in a target message.</param>
         public ProtobufFieldAttribute(uint fieldNumber, 
             DataType dataType = DataType.Default,
             bool packed = false, bool required = false)
@@ -24,21 +34,33 @@ namespace Cloudy.Protobuf.Attributes
             this.dataType = dataType;
         }
 
+        /// <summary>
+        /// Gets the field number in a target message.
+        /// </summary>
         public uint FieldNumber
         {
             get { return fieldNumber; }
         }
 
+        /// <summary>
+        /// Gets whether the field value is packed in a target message.
+        /// </summary>
         public bool Packed
         {
             get { return packed; }
         }
 
+        /// <summary>
+        /// Gets whether the field value is required in a target message.
+        /// </summary>
         public bool Required
         {
             get { return required; }
         }
 
+        /// <summary>
+        /// Gets a way to serialize the property value.
+        /// </summary>
         public DataType DataType
         {
             get { return dataType; }
--- Cloudy/Protobuf/Attributes/ProtobufSerializableAttribute.cs
+++ Cloudy/Protobuf/Attributes/ProtobufSerializableAttribute.cs
@@ -2,6 +2,9 @@
 
 namespace Cloudy.Protobuf.Attributes
 {
+    /// <summary>
+    /// Marks the class as serializable.
+    /// </summary>
     [AttributeUsage(AttributeTargets.Class, AllowMultiple=false, Inherited=true)]
     public class ProtobufSerializableAttribute : Attribute
     {
