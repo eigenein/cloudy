2e3e35f3571cfe44a907dafb49d1797f0ddb7194
Pavel Perestoronin <eigenein@gmail.com>
2011-12-12 02:44:29 +0300

    #18 Fix GetPortScanEndPoints

--- Cloudy/Helpers/IPEndPointExtensions.cs
+++ Cloudy/Helpers/IPEndPointExtensions.cs
@@ -6,10 +6,15 @@ namespace Cloudy.Helpers
 {
     internal static class IPEndPointExtensions
     {
+        public static IPEndPoint Clone(this IPEndPoint endPoint)
+        {
+            return new IPEndPoint(endPoint.Address, endPoint.Port);
+        }
+
         public static IEnumerable<IPEndPoint> GetPortScanEndPoints(
             this IPEndPoint initialEndPoint, int maxOffset)
         {
-            IPEndPoint current = initialEndPoint;
+            IPEndPoint current = initialEndPoint.Clone();
             int nextPortOffset = 0;
             while (Math.Abs(nextPortOffset) <= 2 * maxOffset)
             {
